41740a9a326b56048a88d68ead7ebf466359598a
Ankit Khatiwada <akgunner6@gmail.com>
2023-02-08 22:08:39 +0545

    continue: learning variables and data types in python

---
+++ Day1.py
@@ -0,0 +1,13 @@
+print("Hello Python world!!")
+
+#Variables and Data Types
+message="Hello Python world!!"
+print(message)
+
+message="Hello Python Crash Course world!!"
+print(message)
+
+message_1="Starting with the very basics of Python"
+print(message_1)
+message_1="This is a very good book to start with"
+print(message_1)
